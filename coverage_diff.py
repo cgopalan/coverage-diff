f18fd8d33db116eba1b468425ac3af3ad4f2ed47
Chandrakant Gopalan <chandrakant.gopalan@gmail.com>
2014-04-21 13:39:55 -0400

    Added print formatting and some fixes

--- coverage_diff.py
+++ coverage_diff.py
@@ -1,13 +1,18 @@
 """
-    Determines the difference in statistics between two coverage files.
-    1. For each module in file 1, check the coverage percentage in file2.
-    2. If the module exists, add the coverage percentage of file2.
-    3. If module does not exist, add it to removed modules list.
-    4. If there are modules remaining in file2, add it to new modules list.
-    5. Calculate the diff of the percentages as a new column.
-    6. If diff column is positive, add to increased coverage list.
-    7. If diff column is negative add to decreased coverage list.
+    Determines the difference in statistics between two coverage files,
+    the first arg being the existing coverage file and the second being
+    the new coverage files.
+    Prints the following:
+    As a result of the new coverage file,
+    1. What modules have increased coverage
+    2. What modules have decreased coverage.
+    3. What modules have been added.
+    4. What modules have been removed.
+
+    Things to do:
+    Coverage threshold checks.
 """
+
 
 from pandas.io.parsers import read_fwf
 import sys
@@ -15,28 +20,56 @@ import sys
 
 def get_coverage_dfs(f_cov_exist, f_cov_new):
     """ Gives differential info between two coverage files. """
-    skiprows = get_first_valid_line(f_cov_exist)
-    kwargs = {'widths': [69,5,7,6], 'header': 0,
+
+    kwargs = {'widths': [69, 5, 7, 6], 'header': 0,
               'names': ['Name', 'Stmts', 'Miss', 'Cover'],
-              'skiprows': skiprows,
-              'skipfooter': 6}
-    return (read_fwf(f_cov_exist, **kwargs),
-            read_fwf(f_cov_new, **kwargs))
+              'skipfooter': 6,
+              'index_col': 'Name'}
+
+    kwargs['skiprows'] = get_first_valid_line(f_cov_exist)
+    df_exist = read_fwf(f_cov_exist, **kwargs)
+
+    kwargs['skiprows'] = get_first_valid_line(f_cov_new)
+    df_new = read_fwf(f_cov_new, **kwargs)
+    import ipdb; ipdb.set_trace()
+    return (df_exist, df_new)
 
 
 def get_first_valid_line(f_cov):
     """ Get the first line that has valid data. """
     with open(f_cov) as f:
         for line_no, line in enumerate(f):
-            if line.startswith('-------'):
+            if line.startswith('-'*98):
                 return line_no
         else:
             return 0
 
+
 def print_stats(f_cov_exist, f_cov_new):
     df_exist, df_new = get_coverage_dfs(f_cov_exist, f_cov_new)
+    print("\n")
+    print(" --------- Printing Coverage Diff Stats -------- ")
+    print("\n")
+    print("The following modules have INCREASED coverage now.")
+    print(df_new[df_new.Cover - df_exist.Cover > 0])
+    print("\n")
+    print("The following modules have DECREASED coverage now.")
+    print(df_new[df_new.Cover - df_exist.Cover < 0])
+    print("\n")
+    added = df_new.index -df_exist.index
+    if len(added) > 0:
+        print("The following modules have been added.")
+        for x in added:
+            print(df_new.ix[x])
+        print("\n")
+    removed = df_exist.index - df_new.index
+    if len(removed) > 0:
+        print("The following modules have been removed.")
+        for x in removed:
+            print(df_exist.ix[x])
+        print("\n")
 
 
 if __name__ == '__main__':
-    df_exist, df_new = get_coverage_dfs(sys.argv[1], sys.argv[2])
-    #print_stats(sys.argv[1], sys.argv[1])
+    #df_exist, df_new = get_coverage_dfs(sys.argv[1], sys.argv[2])
+    print_stats(sys.argv[1], sys.argv[2])
